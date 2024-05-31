0af61bb86aaba192543cc698cd4254d5ea1aa2ae
Jacob Hart <>
2024-05-31 14:26:11 +0200

    add sound plot

--- src/routes/explorer/+page.svelte
+++ src/routes/explorer/+page.svelte
@@ -1,10 +1,11 @@
 <script>
     import { base } from '$app/paths';
+    import SoundPlot from '$lib/components/SoundPlot.svelte';
 </script>
 
 <div class="container">
     <div class="inner_container">
-        
+        <SoundPlot />
     </div>
 </div>
 
