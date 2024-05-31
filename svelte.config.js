e81f3a9f11a6464f043fa0187c07fb1234ec825d
Jacob Hart <>
2024-05-31 14:12:35 +0200

    head change

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
     kit: {
         adapter: adapter(),
 		paths : {
-			base: process.env.NODE_ENV === 'production' ? '/jdchart.github.io' : '',
+			base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
 		}
     }
 };
